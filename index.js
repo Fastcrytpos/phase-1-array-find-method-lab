186b558ca744995057c7d09953915b6038658b39
Samson Mubeya <rasspartah@gmail.com>
2024-03-29 14:21:01 +0300

    did my assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function superbowlWin(record) {
+    let year=record.find(findW);
+     if(year){
+        return year.year;}
+        else{
+            return undefined;
+        }
+    }
+
+
+
+
+function findW(records){
+    return records.result==="W";
+}
